1fb8bf07f20432a71a3cb60c2158a64bcd078c71
Jinto Jose <jintoppy@gmail.com>
2018-06-09 10:27:11 +0530

    applying basic style

--- app.ts
+++ app.ts
@@ -19,20 +19,39 @@
         getCartItems(){
 
         }
-        getTotalNumberOfItems(){
-
-        }
         renderCartBox(){
+            const miniCartContainerEl = document.querySelector('.mini-cart');
+            miniCartContainerEl.textContent = `You have ${this.items.length} items in your cart`;
 
         }
     }
 
     class ProductList{
+        private productListContainer: HTMLDivElement;
         constructor(public list: Array<IProduct>){
-
+            this.productListContainer = document.querySelector('.product-list') as HTMLDivElement;
         }
-        renderList(){
+        renderList(): void {
+            this.list.forEach((item: IProduct) => {
+                const productDivEl: HTMLDivElement = document.createElement('div') as HTMLDivElement;
+                productDivEl.className = 'product-item';
+                const productTitleEl : HTMLHeadingElement = document.createElement('h3') as HTMLHeadingElement;
+                const productImageEl : HTMLImageElement = document.createElement('img') as HTMLImageElement;
+                const productPriceEl: HTMLParagraphElement = document.createElement('p') as HTMLParagraphElement;
+                const addProductBtnEl : HTMLButtonElement = document.createElement('button') as HTMLButtonElement;
 
+                productTitleEl.textContent = item.title;
+                productImageEl.src = item.imgUrl;
+                productPriceEl.textContent = item.price.toString();
+                addProductBtnEl.textContent = "Add to Cart";
+
+                productDivEl.appendChild(productTitleEl);
+                productDivEl.appendChild(productImageEl);
+                productDivEl.appendChild(productPriceEl);
+                productDivEl.appendChild(addProductBtnEl);
+
+                this.productListContainer.appendChild(productDivEl);
+            });
         }
     }
 
@@ -63,5 +82,9 @@
         }
     ];
 
+const productList = new ProductList(products);
+const cart = new Cart();
+productList.renderList();
+cart.renderCartBox();
 
 })();
